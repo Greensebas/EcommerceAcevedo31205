9017a4136739a73413a1ce2e7cb1e3725cce2520
Greensebas <greensebas@hotmail.com>
2022-05-25 18:46:42 -0300

    255

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import NavBar from './NavBar';
 function App() {
   return (
   <div>
-    <h1>Esto es un título</h1>
     <NavBar />
+    <h1>Esto es un título</h1>
   </div>
   );
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,36 @@
 function NavBar() {
-    return <h1>Esto es una NavBar</h1>;
+    return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="google.com">
+        <img src="img/wapp.png" alt="" width="50" height="56"></img>
+      </a>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="google.com">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="google.com">Noticias</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="google.com" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Productos
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a className="dropdown-item" href="google.com">Producto 1</a></li>
+              <li><a className="dropdown-item" href="google.com">Producto 2</a></li>
+              <li><a className="dropdown-item" href="google.com">Producto 3</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="google.com">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   }
   
   export default NavBar;
   
+    
