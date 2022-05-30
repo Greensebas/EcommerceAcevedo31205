68a90372b93dbb992271c62a700db8f081627676
Greensebas <greensebas@hotmail.com>
2022-05-30 00:34:42 -0300

    heroPrueba

--- src/components/Hero/HeroLayout.jsx
+++ src/components/Hero/HeroLayout.jsx
@@ -3,11 +3,9 @@ import './Hero.css'
 
 function HeroLayout() {
   return (
-    <div className="container">
       <div className="hero">
-        <h1 className='texto'>FIODORA</h1>
+        <h1 className='texto'>Fiodora</h1>
       </div>
-    </div>
   )
 }
 
