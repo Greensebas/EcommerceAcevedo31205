fcacf02acd44a634eba82d79755df2ab4b2b5726
Greensebas <greensebas@hotmail.com>
2022-07-05 22:19:12 -0300

    formulario formik

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,43 +1,63 @@
 import React from "react";
+import { Formik } from "formik";
 import "./form.css";
 
 function Form() {
   return (
     <>
-      <div className="container top-navbar py-5">
-        <div className="row">
-          <form className=" formulario">
-            <div>
-              <label htmlFor="nombre">Nombre</label>
-              <input
-                type="text"
-                id="nombre"
-                name="nombre"
-                placeholder="Ingresa tu nombre y apellido"
-              />
+      <Formik
+        initialValues={{
+          nombre: "",
+          email: "",
+          cel: ""
+        }}
+        onSubmit={() => {
+          console.log("formulario enviado")
+        }}
+      >
+        {({values, handleSubmit, handleChange}) => (
+          <div className="container top-navbar py-5">
+            <div className="row">
+              <form className=" formulario" onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="nombre">Nombre</label>
+                  <input
+                    type="text"
+                    id="nombre"
+                    name="nombre"
+                    placeholder="Ingresa tu nombre y apellido"
+                    value={values.nombre}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="mail">Mail</label>
+                  <input
+                    type="text"
+                    id="mail"
+                    name="mail"
+                    placeholder="Ingresa tu correo electrónico"
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="cel">Cel</label>
+                  <input
+                    type="text"
+                    id="cel"
+                    name="cel"
+                    placeholder="Ingresa un teléfono de contacto"
+                    value={values.cel}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button type="submit">TERMINAR COMPRA</button>
+              </form>
             </div>
-            <div>
-              <label htmlFor="mail">Mail</label>
-              <input
-                type="email"
-                id="mail"
-                name="mail"
-                placeholder="Ingresa tu correo electrónico"
-              />
-            </div>
-            <div>
-              <label htmlFor="cel">Cel</label>
-              <input
-                type="text"
-                id="cel"
-                name="cel"
-                placeholder="Ingresa un teléfono de contacto"
-              />
-            </div>
-            <button type="submit">TERMINAR COMPRA</button>
-          </form>
-        </div>
-      </div>
+          </div>
+        )}
+      </Formik>
     </>
   );
 }
