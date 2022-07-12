ec2c5be1e011bedc4755d5d46aaef381b06551a7
Greensebas <greensebas@hotmail.com>
2022-07-12 16:07:03 -0300

    asdf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='container-fluid bg-light py-2'>
-      <div className='container'>
-        <div>ACA VA A IR TREMENDO FOOTER</div>
+    <>
+    <div className="container-fluid bg-light p-5">
+      <div className="container">
+      <div className="row ">
+            <div className="col-xs-12 col-md 6 col-lg-3 text-center">
+              <img src="img/FIODORA-PNG.png" alt="" width="60" height="90"></img>
+              <p className="h6">DISEÑO GRÁFICO</p>
+            </div>
+            <div className="col-xs-12 col-md 6 col-lg-3">
+              <p className="h5">Productos</p>
+            </div>
+            <div className="col-xs-12 col-md 6 col-lg-3">
+              <p className="h5">Contacto</p>
+            </div>
+            <div className="col-xs-12 col-md 6 col-lg-3">
+              <p className="h5">Columna</p>
+            </div>
       </div>
-
-    </footer>
-
-  )
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
